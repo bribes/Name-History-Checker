6bcaaca3e90078ee6486b89d7c8576ba1137615c
Faav <52789876+withdrew@users.noreply.github.com>
2021-04-18 19:39:14 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,35 +1,33 @@
 /*jshint esversion: 6 */
 //Gets a query
-function getQueryVariable(variable) {
-	var query = window.location.search.substring(1);
-	var vars = query.split("&");
-	for (var i = 0; i < vars.length; i++) {
-		var pair = vars[i].split("=");
-		if (pair[0] == variable) {
-			return pair[1];
-		}
-		return "";
-	}
+function getUsername() {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == 'username') {
+            var username = pair[1].split('+').join('%20');
+            return username;
+        }
+        return "";
+    }
 }
 //Formats the dates
 function formatTime(timestamp) {
-	var date = new Date(timestamp);
-	return date.toLocaleString();
+    var date = new Date(timestamp).toLocaleString();
+    return date;
 }
-//HTML Entities
-function htmlEntities(str) {
-	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-var username = htmlEntities(decodeURIComponent(getQueryVariable('username'))); //Username query
+var username = decodeURIComponent(getUsername()); //Username query
 var error_message = "No minecraft account currently has that username!"; //The Error Message
 var API_URL = "https://www.faav.tk/v1/namemc/namehistory?username="; //The API URL
+var API = API_URL + getUsername(); //Full API URL (DONT EDIT)
 var input = document.getElementById('username'); //The input
-input.value = decodeURIComponent(getQueryVariable('username')); //Sets the input value to the username
-fetch(API_URL + username).then(response => response.json()).then((data) => {
-	if (username !== '') { //Checks if the username isn't blank
-		if (data.error == "This user doesn't exist") { //Checks if the username is on a account
-			document.getElementById('myTable').innerHTML = '<td>' + error_message + '</td>'; //Makes the error message
-		} else {
+input.value = username; //Sets the input value to the username
+fetch(API).then(response => response.json()).then((data) => {
+    if (username !== '') { //Checks if the username isn't blank
+        if (data.error == "This user doesn't exist") { //Checks if the username is on a account
+            document.getElementById('myTable').innerHTML = '<td>' + error_message + '</td>'; //Makes the error message
+        } else {
 			buildTable(data); //Makes the Name History
 		}
 	}
