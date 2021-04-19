895edfd55312579c80e7d2c703c330ca0de4a131
Faav <52789876+withdrew@users.noreply.github.com>
2021-04-19 13:01:28 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -14,45 +14,46 @@ function getUsername() {
 }
 //Formats the dates
 function formatTime(timestamp) {
-    var date = new Date(timestamp).toLocaleString();
-    return date;
+    var date = new Date(timestamp);
+    return date.toLocaleString();
 }
 var username = decodeURIComponent(getUsername()); //Username query
 var error_message = "No minecraft account currently has that username!"; //The Error Message
-var API_URL = "https://www.faav.tk/v1/namemc/namehistory?username="; //The API URL
-var API = API_URL + getUsername(); //Full API URL (DONT EDIT)
+var API_URL = "https://playerdb.co/api/player/minecraft/"; //The API URL
+var API = API_URL + username; //Full API URL (DONT EDIT)
 var input = document.getElementById('username'); //The input
 input.value = username; //Sets the input value to the username
 fetch(API).then(response => response.json()).then((data) => {
     if (username !== '') { //Checks if the username isn't blank
-        if (data.error == "This user doesn't exist") { //Checks if the username is on a account
+        if (data.error === true) { //Checks if there is a error
             document.getElementById('myTable').innerHTML = '<td>' + error_message + '</td>'; //Makes the error message
+            console.clear();
         } else {
-			buildTable(data); //Makes the Name History
-		}
-	}
-	//Name History Section
-	function buildTable(data) {
-		var table = document.getElementById('myTable');
-		if (formatTime(data[0].timestamp) == "12/31/1969, 7:00:00 PM" || formatTime(data[0].timestamp) == "1/1/1970, 1:00:00 AM") {
-			var row = `<tr class="bold">
-	  <td>` + data.length + `. <a href="?username=` + data[0].username + `">` + data[0].username + `</a><\/td>`;
-			table.innerHTML += row;
-		} else {
-			var row = `<tr class="bold">
-	  <td>` + data.length + `. <a href="?username=` + data[0].username + `">` + data[0].username + `</a><\/td><td class="right">` + formatTime(data[0].timestamp) + `<\/td>
+            buildTable(data.data.player.meta.name_history.reverse()); //Makes the Name History
+        }
+    }
+    //Name History Section
+    function buildTable(data) {
+        var table = document.getElementById('myTable');
+        if (data.length === 1) {
+            var row = `<tr class="bold">
+	  <td>` + data.length + `. <a href="?username=` + data[0].name + `">` + data[0].name + `</a><\/td>`;
+            table.innerHTML += row;
+        } else {
+            var row = `<tr class="bold">
+	  <td>` + data.length + `. <a href="?username=` + data[0].name + `">` + data[0].name + `</a><\/td><td class="right">` + formatTime(data[0].changedToAt) + `<\/td>
 	                     <\/tr>`;
-			table.innerHTML += row;
-			for (var i = 1; i < data.length - 1; i++) {
-				var row1 = `<tr>
-	  <td>` + data[i].order + `. <a href="?username=` + data[i].username + `">` + data[i].username + `<\/a><\/td><td class="right">` + formatTime(data[i].timestamp) + `<\/td>
+            table.innerHTML += row;
+            for (var i = 1; i < data.length - 1; i++) {
+                var row1 = `<tr>
+	  <td>` + (data.length - i) + `. <a href="?username=` + data[i].name + `">` + data[i].name + `<\/a><\/td><td class="right">` + formatTime(data[i].changedToAt) + `<\/td>
 	                     <\/tr>`;
-				table.innerHTML += row1;
-			}
-			var row2 = `<tr>
-	  <td>` + data[i].order + `. <a href="?username=` + data[i].username + `">` + data[i].username + `</a><\/td>
+                table.innerHTML += row1;
+            }
+            var row2 = `<tr>
+	  <td>` + (data.length - i) + `. <a href="">` + data[i].name + `</a><\/td>
 	                     <\/tr>`;
-			table.innerHTML += row2;
-		}
-	}
+            table.innerHTML += row2;
+        }
+    }
 });
