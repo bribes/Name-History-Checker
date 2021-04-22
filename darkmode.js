b68944e31c8e159df7bf6a68c5f4676651ca5f72
Faav <52789876+withdrew@users.noreply.github.com>
2021-04-22 12:15:41 -0400

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,23 @@
+ let mode;
+	      mode = localStorage.getItem('mode');
+
+	      if (mode === 'light'){
+	         lightMode();
+	      }else{
+	         darkMode();
+	      }
+	      function lightMode() {
+	      document.body.className = "lightmode-background";
+	         document.getElementById("darkmodebtn").className = "lightmode-button";
+		 document.getElementById("darkmodebtn").onclick = darkMode;
+	         localStorage.setItem('mode', 'light');
+	         mode = localStorage.getItem('mode');
+	      }
+
+	      function darkMode() {
+	      document.body.className = "darkmode-background";
+	         document.getElementById("darkmodebtn").className = "darkmode-button";
+		 document.getElementById("darkmodebtn").onclick = lightMode;
+	         localStorage.setItem('mode', 'dark');
+	         mode = localStorage.getItem('mode');
+	      }
