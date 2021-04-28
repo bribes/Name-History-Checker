a6d4ca8ac97314cad31ecede4d513ff6de76861f
Faav <52789876+withdrew@users.noreply.github.com>
2021-04-27 20:48:19 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -72,7 +72,7 @@ if (username !== "") { //Checks if the username isn"t blank
     } else {
       window.username = main.data.player.username;
       window.name_history = main.data.player.meta.name_history.reverse();
-      var icon = main.data.player.avatar + "?overlay"; // The Favicon
+      var icon = "https://api.ashcon.app/mojang/v2/avatar/" + window.username; // The Favicon
       var title = window.username + " | Name History"; // The Title
       buildTable(window.name_history); //Makes the Name History
       document.getElementsByTagName("title")[0].innerText = title; //Adds the Title
